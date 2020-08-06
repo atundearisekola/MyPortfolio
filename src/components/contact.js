ef80c9613f3f520262b761df0e8c3fe375577e6e
atundearisekola <atundearisekola@yahoo.com>
2020-08-06 16:17:57 +0100

    background update

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,58 +1,65 @@
-import React,{Component} from 'react';
-import { Grid, Cell,List,ListItem,ListItemContent } from 'react-mdl';
-import atunde from '../images/atunde.jpg';
-class Contact extends Component{
-    render(){
-        return(
-            <div className="contact-body">
-                <Grid className="contact-grid">
-                    <Cell col={6}>
-                        <h2>Atunde Arisekola</h2>
-                        <img 
-                        style={{height: '250px'}}
-                        src={atunde}
-                        alt="avatar"
-                        className="avatar-img"/>
-                        <p style={{width: '750', margin: 'auto', paddingTop: '1em'}}>
-                        Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.
-                        </p>
+import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+import atunde from '../images/myimage.jpg';
+class Contact extends Component {
+  render() {
+    return (
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Atunde Arisekola</h2>
+            <img
+              style={{ height: '250px' }}
+              src={atunde}
+              alt="avatar"
+              className="avatar-img"
+            />
+            <p style={{ width: '750', margin: 'auto', paddingTop: '1em' }}>
+              Lorem ipsum, or lipsum as it is sometimes known, is dummy text
+              used in laying out print, graphic or web designs. The passage is
+              attributed to an unknown typesetter in the 15th century who is
+              thought to have scrambled parts of Cicero's De Finibus Bonorum et
+              Malorum for use in a type specimen book.
+            </p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: '18px', fontFamily: 'Anton' }}
+                  >
+                    <i className="fa fa-phone-square" aria-hidden="true" />
+                    (234) 8159109387
+                  </ListItemContent>
+                </ListItem>
 
-                    </Cell>
-                    <Cell col={6}>
-                        <h2>Contact Me</h2>
-                        <hr/>
-                        <div className="contact-list">
-                        <List>
-  <ListItem>
-    <ListItemContent  style={{fontSize: '18px', fontFamily: 'Anton'}}>
-        <i className="fa fa-phone-square" aria-hidden="true" />
-        (234) 8159109387
-        </ListItemContent>
-  </ListItem>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: '18px', fontFamily: 'Anton' }}
+                  >
+                    <i className="fa fa-envelope" aria-hidden="true" />
+                    atundearisekola@gmail.com
+                  </ListItemContent>
+                </ListItem>
 
-  <ListItem>
-    <ListItemContent style={{fontSize: '18px', fontFamily: 'Anton'}}>
-        <i className="fa fa-envelope" aria-hidden="true" />
-        atundearisekola@gmail.com
-        </ListItemContent>
-  </ListItem>
-
-  <ListItem>
-    <ListItemContent style={{fontSize: '18px', fontFamily: 'Anton'}}>
-        <i className="fa fa-skype" aria-hidden="true" />
-        @atundearisekola
-        </ListItemContent>
-  </ListItem>
- 
-</List>
-
-                        </div>
-                    </Cell>
-                
-                </Grid>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: '18px', fontFamily: 'Anton' }}
+                  >
+                    <i className="fa fa-skype" aria-hidden="true" />
+                    @atundearisekola
+                  </ListItemContent>
+                </ListItem>
+              </List>
             </div>
-        )
-    }
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 
-export default Contact; 
+export default Contact;
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,112 +1,66 @@
-import React,{Component} from 'react';
-import { Grid ,Cell,Chip,ChipContact} from 'react-mdl';
-import atunde from '../images/atunde.jpg';
-class Landing extends Component{
-    render(){
-        return(
-            <div style={{width: '100%', margin:'auto'}}>
-                <Grid className="landing-grid">
-                    <Cell col={12}>
-                        <img 
-                        src={atunde}
-                        alt="avatar"
-                        className="avatar-img"/>
-                        <div className="banner-text">
-                            <h1>Full Stack Developer</h1>
+import React, { Component } from 'react';
+import { Grid, Cell, Chip, ChipContact } from 'react-mdl';
+import atunde from '../images/myimage.jpg';
+class Landing extends Component {
+  render() {
+    return (
+      <div style={{ width: '100%', margin: 'auto' }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img src={atunde} alt="avatar" className="avatar-img" />
+            <div className="banner-text">
+              <h1>Full Stack Developer</h1>
 
-                            <hr/>
+              <hr />
 
-                            <p>
-                            <Chip>
-    <ChipContact  className="mdl-color--teal mdl-color-text--white"></ChipContact>
-    PHP/Laravel
-</Chip>
-
-<Chip>
-<ChipContact  className="mdl-color--teal mdl-color-text--white"></ChipContact>
-    Python
-</Chip> 
-<Chip>
-<ChipContact  className="mdl-color--teal mdl-color-text--white"></ChipContact>
-    JAVA
-</Chip>
-<Chip>
-<ChipContact  className="mdl-color--teal mdl-color-text--white"></ChipContact>
-Javascript
-</Chip>  
-<Chip>
-<ChipContact  className="mdl-color--teal mdl-color-text--white"></ChipContact>
-React
-</Chip>  
-<Chip>
-<ChipContact  className="mdl-color--teal mdl-color-text--white"></ChipContact>
-React Native 
-</Chip> 
-<Chip>
-<ChipContact  className="mdl-color--teal mdl-color-text--white"></ChipContact>
-Javascript
-</Chip> 
-<Chip>
-<ChipContact  className="mdl-color--teal mdl-color-text--white"></ChipContact>
-NodeJS 
-</Chip>  
-<Chip>
-<ChipContact  className="mdl-color--teal mdl-color-text--white"></ChipContact>
-Express
-</Chip>   
-<Chip>
-<ChipContact  className="mdl-color--teal mdl-color-text--white"></ChipContact>
-MongoDB
-</Chip>   
-<Chip>
-<ChipContact  className="mdl-color--teal mdl-color-text--white"></ChipContact>
-Firebase
-</Chip>   
-<Chip>
-<ChipContact  className="mdl-color--teal mdl-color-text--white"></ChipContact>
-SQL
-</Chip>  </p>
-                            <Grid className="social-links">
-                                <Cell col={2} phone={6} tablet={4}>
-                                     {/* LinkedIn*/}
-                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-linkedin-square"  areia-hidden="true" />
-                                </a>
-                                </Cell>
-                                <Cell col={2} phone={6} tablet={4}>
-                                     {/* Github*/}
-                                <a href="http://github.com/atundearisekola" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-github-square"  areia-hidden="true" />
-                                </a>
-
-                                </Cell>
-                                <Cell col={2} phone={6} tablet={4}>
-                                    {/* freecodecamp*/}
-                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-free-code-camp"  areia-hidden="true" />
-                                </a>
-
-                                    
-                                </Cell>
-                                <Cell col={2} phone={6} tablet={4}>
-                                     {/* Youtube*/}
-                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-youtube-square"  areia-hidden="true" />
-                                </a>
-                                    
-                                </Cell>
-                            </Grid>
-                            
-
-                        </div>
-                        
-
-                    </Cell>
-                </Grid>
-
+              <Grid className="social-links">
+                <Cell col={2} phone={6} tablet={4}>
+                  {/* LinkedIn*/}
+                  <a
+                    href="http://google.com"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <i className="fa fa-linkedin-square" areia-hidden="true" />
+                  </a>
+                </Cell>
+                <Cell col={2} phone={6} tablet={4}>
+                  {/* Github*/}
+                  <a
+                    href="http://github.com/atundearisekola"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <i className="fa fa-github-square" areia-hidden="true" />
+                  </a>
+                </Cell>
+                <Cell col={2} phone={6} tablet={4}>
+                  {/* freecodecamp*/}
+                  <a
+                    href="http://google.com"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <i className="fa fa-free-code-camp" areia-hidden="true" />
+                  </a>
+                </Cell>
+                <Cell col={2} phone={6} tablet={4}>
+                  {/* Youtube*/}
+                  <a
+                    href="http://google.com"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <i className="fa fa-youtube-square" areia-hidden="true" />
+                  </a>
+                </Cell>
+              </Grid>
             </div>
-        )
-    }
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Landing;
